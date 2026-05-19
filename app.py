37950a919363becb02b03369aa1d80d29b5eff4a
IRIANNY24 <iriannisardila@gmail.com>
2026-05-18 21:20:21 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+modelo = joblib.load("modelos/modelo_logistico.pkl")
+
+st.title("Predicción de Diabetes")
+
+st.write("Nombre: Irianny Ardila")
+st.write("Código ISIL: TU_CODIGO")
+
+st.write("Link del COLAB:")
+st.write("PEGA_AQUI_TU_LINK")
+
+pregnancies = st.number_input("Pregnancies")
+glucose = st.number_input("Glucose")
+bloodpressure = st.number_input("BloodPressure")
+skinthickness = st.number_input("SkinThickness")
+insulin = st.number_input("Insulin")
+bmi = st.number_input("BMI")
+dpf = st.number_input("DiabetesPedigreeFunction")
+age = st.number_input("Age")
+
+if st.button("Predecir"):
+
+    datos = np.array([[pregnancies, glucose, bloodpressure,
+                       skinthickness, insulin, bmi, dpf, age]])
+
+    prediccion = modelo.predict(datos)
+
+    if prediccion[0] == 1:
+        st.error("La persona tiene diabetes")
+    else:
+        st.success("La persona no tiene diabetes")
